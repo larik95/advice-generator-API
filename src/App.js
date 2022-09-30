a34a306c7806e5af9527b2b5bf3f37fc4650c7c7
Larissa <larissaferreira_@live.com>
2022-09-30 19:12:10 -0300

    Advice generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import dice from './desing/icon-dice.svg';
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState (null); 
+  const [adviceId, setAdviceId] = useState(null);
+  const getData = async () => {
+    const { data } = await axios.get("https://api.adviceslip.com/advice");
+    setAdvice(data.slip.advice);
+    setAdviceId(data.slip.id);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <div className='container'>
+        <p className='container-title'> Advice #{adviceId}</p>
+        <p className='container-advice'> "{advice}" </p>
+        <div className='container-divider'></div>
+        <div className='container-dice' onClick={() => getData()}>
+          <img src={dice} alt="dice" />
+        </div>
+      </div>
+      </main>
     </div>
   );
 }
